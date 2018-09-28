9a4355e67cbec5595d0fd06e94085439cecc9d84
deeelan <dilanmolano@gmail.com>
2018-09-27 22:37:13 -0500

    Implementación de tienda de revistas online

---
+++ Revistas/src/subject.js
@@ -0,0 +1,16 @@
+function searchTupla() {
+	var search = document.getElementById('search').value;
+	var dataEn = 'search='+search;
+}
+
+function insertUser() {
+	var identification = document.getElementById('identification').value;
+	var first_name = document.getElementById('first_name').value;
+	var last_name = document.getElementById('last_name').value;
+	var age = document.getElementById('age').value;
+	var city = document.getElementById('city').value;
+}
+
+function () {
+	// body...
+}
